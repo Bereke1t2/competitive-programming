edb32383907b58d19c492750c515ab418905adab
Bereket Aschalew <bereketaschalew1212@gmail.com>
2025-03-24 07:12:39 +0000

    Time: 2 ms (78.25%), Space: 17.8 MB (64.34%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,17 +1,22 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        def dfs(prefix, n, k):
-            if n == 0:
-                return prefix
-            for c in 'abc':
-                if prefix and c == prefix[-1]:
-                    continue
-                cnt = 2 ** (n2 - len(prefix) - 1)
-                if cnt >= k:
-                    return dfs(prefix + c, n - 1, k)
-                else:
-                    k -= cnt
-            return ""
         
-        n2 = n
-        return dfs("", n, k)
+        count = 0
+        res = ''
+        def backtrack(path):
+            nonlocal count , n , k , res
+            if count==k:
+                return
+            if len(path)==n:
+                count +=1
+                if count==k:
+                    res = ''.join(path)
+                return
+            for l in 'abc':
+                if not path or path[-1]!=l:
+                    path.append(l)
+                    backtrack(path)
+                    path.pop()
+        backtrack([])
+        return res
+            
