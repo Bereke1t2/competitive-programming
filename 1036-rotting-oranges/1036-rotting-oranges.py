a9a154d1dd3e85cba08e939111361e8fa1fe8984
Bereket Aschalew <bereketaschalew1212@gmail.com>
2025-04-15 16:52:57 +0000

    Time: 11 ms (6.74%), Space: 18.1 MB (6.75%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -8,38 +8,34 @@ class Solution:
                 temp.append(d[grid[i][j]])
             mat.append(temp.copy())
 
-
+        found = False 
+        q=deque([])   
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==2: 
+                    q.append((i, j, 0))
+                    found  = True
+                if grid[i][j]==1:
+                    found = True
+        
         rows , cols = len(grid) , len(grid[0])
         directions_straight = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         def is_inbound(x, y):
             return 0 <= x < rows and 0 <= y < cols
+    
+        seen = defaultdict(int)
+        while q:
+            x , y , time = q.popleft()
+            # print(x,y,time)
+            mat[x][y] = min(mat[x][y] , time)
 
-        def bfs(i , j):
-            seen = defaultdict(int)
+            time +=1
+            for dx , dy in directions_straight:
+                new_x  , new_y = x+dx  , y + dy
+                if is_inbound(new_x,new_y) and grid[new_x][new_y]==1 and not (new_x ,new_y) in seen:
+                    q.append((new_x, new_y,time))
+                    seen[(new_x ,new_y)] +=1
 
-            q = deque([(i, j , 0)])
-
-            while q:
-                x , y , time = q.popleft()
-                # print(x,y,time)
-                mat[x][y] = min(mat[x][y] , time)
-
-                time +=1
-                for dx , dy in directions_straight:
-                    new_x  , new_y = x+dx  , y + dy
-                    if is_inbound(new_x,new_y) and grid[new_x][new_y]==1 and not (new_x ,new_y) in seen:
-                        q.append((new_x, new_y,time))
-                        seen[(new_x ,new_y)] +=1
-
-        found = False
-            
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]==2:
-                    bfs(i,j)
-                    found  = True
-                if grid[i][j]==1:
-                    found = True
         if not found:
             return 0
         max_ = max([max(nums) for nums in mat])
