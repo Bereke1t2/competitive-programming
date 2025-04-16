bb0bb413a52f59d12193589d2ad794a93e2d6678
Bereket Aschalew <bereketaschalew1212@gmail.com>
2025-04-16 18:34:52 +0000

    Time: 66 ms (93.54%), Space: 37.9 MB (83.98%) - LeetHub

---
+++ 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSumBST(self, root: TreeNode) -> int:
+        res = 0
+        def traverse(root):
+            nonlocal res
+            if not root: return 1, 0, None, None
+            
+            ls, l, ll, lr = traverse(root.left)
+            rs, r, rl, rr = traverse(root.right)
+            
+            if ((ls == 2 and lr < root.val) or ls == 1) and ((rs == 2 and rl > root.val) or rs == 1):
+                size = root.val + l + r
+                res = max(res, size)
+                return 2, size, (ll if ll is not None else root.val), (rr if rr is not None else root.val)
+            return 0, None, None, None 
+        
+        traverse(root)
+        return res
