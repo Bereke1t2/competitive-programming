1629587b329a4547ad46b50e359aa08fbfa79460
Bereket Aschalew <bereketaschalew1212@gmail.com>
2026-03-14 12:37:34 +0000

    Time: 0 ms (100.00%) | Memory: 19.4 MB (54.55%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = defaultdict(int)
+
+        def dp(i):
+            if i<0:
+                return 0
+            if i in memo:
+                return memo[i]
+            
+            memo[i] = max(dp(i-1) , dp(i-2) + nums[i])
+            return memo[i]
+        return dp(len(nums)-1)
